e84062c88056f7e8d2ff9c53900d8edd54822aba
Uemmra3 <Uemmra3@gmail.com>
2018-02-06 02:29:49 +0900

    変数名などのリファクタリング

--- docs/main.js
+++ docs/main.js
@@ -7,20 +7,73 @@
 //            時計の進め方を調節
 //            インデントを調整
 ///////////////////////////////////////
-power = 0;
-tim = st = ave = trp = rc = k = tp = smp = f = map = fn = n = l = m = ent = step = s = s10 = d = f2 = p = dir = trc = et = ss = '';
-ch =  new Array(3, 0, 0, 3, 0, 2, 0, 3);
-strp =  new Array(0, 0, 0);
+//"use strict";
+// リファクタリング時には、htmlのonClick呼び出しで変数名を指定しているので、合わせて変更すること
+var power_ = 0; // 電源状態
+
+// ラリコンが計算に使う各種の値(オブジェクト化すべきなのかも？)
+var time_ = ''; // 時刻
+var startTime_ = '';    // スタートタイム
+var ave_ = '';  // アベ
+var trp_ = '';  // トリップ(前のPCやCPからの距離)
+var strp_ =  [0, 0, 0]; // トリップ履歴
+var resCon_ = '';   // レスコン
+var kHosei_ = '';   // K(補正値)
+var tp = '';    // 距離補正用
+var f = ''; // ファイナル
+var trc = '';   // レスコン調整用
+var map_ = '';  // map値(コマ図距離計測用)
+var smp = '';   // ストアmap(前回のmap)
+var tripSensorDir_ = '';  // トリップセンサーの方向
+
+// 操作関連の変数値
+var fn = '';    // FN が押されているか？のステータス
+var n = ''; // 入力する数値
+var p = ''; // ピリオド入力中なのかのステータス
+var l = ''; // 現在選択中のファンクション
+var targetSelector_ = ''; // 入力対照のファンクション
+var enterType_ = '';  // 入力モードの種類　(0: 通常, 1: , 2: オド, 3: CP, 4: PC)
+var enterSteps_ = '';   // 入力モードのステップ数
+var et_ = '';    // 入力が続くか/値の表示のみかのフラグ？
+// タイマー用
+var s = '';
+var s10 = '';
+var ss = '';
+var d = '';
+var f2Mode_ = '';   // F2モードの切り替え(通常: 0, F2モード: 1)
+
+// 
+var ch = [3, 0, 0, 3, 0, 2, 0, 3];
+
 //
 function onoff() {         //電源スイッチ//
-  if (power > 0) {
+  if (power_ > 0) {
     //power = 0;
     return;
   }
 
-  power = k = dir = 1;
-  ave = 30;
-  tim = st = trp = rc = tp = smp = f = map = fn = l = m = ent = step = f2 = p = trc = et = ss = 0;
+  power_ = 1;
+  kHosei_ = 1;
+  tripSensorDir_ = 1;
+  ave_ = 30;
+  time_ = 0;
+  startTime_ = 0;
+  trp_ = 0;
+  resCon_ = 0;
+  tp = 0;
+  smp = 0;
+  f = 0;
+  map_ = 0;
+  fn = 0;
+  l = 0;
+  targetSelector_ = 0;
+  enterType_ = 0;
+  enterSteps_ = 0;
+  f2Mode_ = 0;
+  p = 0;
+  trc = 0;
+  et_ = 0;
+  ss = 0;
   document.NONO.funct.value = "0";
   s10 = setTimeout("chousei()", 6000);
   s = setTimeout("second()", 1000);
@@ -55,30 +108,30 @@ function chousei() {         //６秒毎の時間調整//
 }
 //
 function hyouji() {         //時刻処理//
-  tim++;
-  tim %= 43200;
-  if (m == 0) {
-    d = clock(tim);
-    if (step == 0 && f2 == 0) {
+  time_++;
+  time_ %= 43200;
+  if (targetSelector_ == 0) {
+    d = clock(time_);
+    if (enterSteps_ == 0 && f2Mode_ == 0) {
       document.NONO.funct.value = d;
     }
   }
-  if (f2 == 0) {
-    if (power > 0 && ent != 3) {         //ＣＰ処理以外でファイナル更新
-      f = st + (3600 * trp / ave / 1000) - tim + trc;
+  if (f2Mode_ == 0) {
+    if (power_ > 0 && enterType_ != 3) {         //ＣＰ処理以外でファイナル更新
+      f = startTime_ + (3600 * trp_ / ave_ / 1000) - time_ + trc;
       f = (f + 64800) % 43200 - 21600;
       f = kirisute(f, 1);
     }
     document.NONO.finalsc.value = clock(f) + (fn == 1 ? "   == Fn == " : "");
-    document.NONO.mapsc.value = map + "      " + "+N-".charAt(1 - dir);
+    document.NONO.mapsc.value = map_ + "      " + "+N-".charAt(1 - tripSensorDir_);
   }
-  document.NONO.lt[m].checked = true;
-  if (step > 0) {
+  document.NONO.lt[targetSelector_].checked = true;
+  if (enterSteps_ > 0) {
     ss = setTimeout("tennmetsu()", 500);
   }
 
-  if (l == 3 && step == 0 && f2 == 0) {
-    document.NONO.funct.value = trp;
+  if (l == 3 && enterSteps_ == 0 && f2Mode_ == 0) {
+    document.NONO.funct.value = trp_;
   }
 ///// for Debug
 //    window.status  =  addzero(tim)+
@@ -100,14 +153,14 @@ function hyouji() {         //時刻処理//
 }
 //
 function tennmetsu() {         //エントリー項目点滅//
-  document.NONO.lt[m].checked = false;
-  if (ent == 3) {
+  document.NONO.lt[targetSelector_].checked = false;
+  if (enterType_ == 3) {
     document.NONO.finalsc.value = "";
   }
 }
 //
 function display(funct, num) {         //FUNCT欄に表示する//
-  if (power > 0) {
+  if (power_ > 0) {
     funct.value = num;
   }
 }
@@ -120,8 +173,8 @@ function clock(t) {         //時分秒表示にする//
 }
 //
 function addzero(u) {         //０をくっつけ６桁化//
-  v = u;
-  for (i = 10; i < 100000; i *= 10) {
+  var v = u;
+  for (var i = 10; i < 100000; i *= 10) {
     v = (u * u < i * i ? "0" : "") + v;
   }
   v = ((u >= 0 && u < 100000) ? "0" : "") + v;
@@ -129,56 +182,57 @@ function addzero(u) {         //０をくっつけ６桁化//
 }
 //
 function push(button) {         //ボタン処理//
-  if (power <= 0) {
+  if (power_ <= 0) {
     return;
   }
 
   if (fn == 1) {
-    if (button == 1 && ent == 3 && step == 3) {         //オド処理
-      ent = 2;
-      step = 4;
-      m = 6;
-      et = 1;
-      f2 = fn = 0;
+    if (button == 1 && enterType_ == 3 && enterSteps_ == 3) {         //オド処理
+      enterType_ = 2;
+      enterSteps_ = 4;
+      targetSelector_ = 6;
+      et_ = 1;
+      f2Mode_ = 0;
+      fn = 0;
       cl();
       fu(16);
     }
     if (button == 2) {         //Ｆ２モード切替
-      if (f2 == 0 && step == 0) {
-        f2 = 1;
-        document.NONO.lt[m].checked = false;
-        document.NONO.funct.value = strp[0] + "   F2mode";
-        document.NONO.finalsc.value = strp[1];
-        document.NONO.mapsc.value = strp[2];
+      if (f2Mode_ == 0 && enterSteps_ == 0) {
+        f2Mode_ = 1;
+        document.NONO.lt[targetSelector_].checked = false;
+        document.NONO.funct.value = strp_[0] + "   F2mode";
+        document.NONO.finalsc.value = strp_[1];
+        document.NONO.mapsc.value = strp_[2];
         fu(17);
       } else {
-        f2 = 0;
-        document.NONO.lt[m].checked = true;
+        f2Mode_ = 0;
+        document.NONO.lt[targetSelector_].checked = true;
         display(document.NONO.funct, d);
       }
     }
     if (button == 3) {         //向き変更
-      i = (dir == 1 ? -1 : 0) + (dir == 0 ? 1 : 0);
-      dir = i;
+      var i = (tripSensorDir_ == 1 ? -1 : 0) + (tripSensorDir_ == 0 ? 1 : 0);
+      tripSensorDir_ = i;
       fu(18);
     }
     if (button == 5) {         //レスコン
-      trc += rc;
+      trc += resCon_;
       fu(19);
     }
     if (button == 6) {         //レスコン
-      trc -= rc;
+      trc -= resCon_;
       fu(20);
     }
     if (button == 8) {         //プラス補正
-      if (step == 0) {
-        trp += tp;
+      if (enterSteps_ == 0) {
+        trp_ += tp;
         fu(21);
       }
     }
     if (button == 9) {         //マイナス補正
-      if (step == 0) {
-        trp -= tp;
+      if (enterSteps_ == 0) {
+        trp_ -= tp;
         fu(22);
       }
     }
@@ -187,7 +241,8 @@ function push(button) {         //ボタン処理//
     }
     fn = 0;
   } else {         //入力処理
-    if ((power > ch[l] && step == 0 && f2 == 0) || (step > 0 && et == 1)) {
+    if ((power_ > ch[l] && enterSteps_ == 0 && f2Mode_ == 0)
+        || (enterSteps_ > 0 && et_ == 1)) {
       if (n == "undefined") {//
         n = "";//
       }//
@@ -200,8 +255,8 @@ function push(button) {         //ボタン処理//
           n += "" + button;
         }
       }
-      ndis = ((ent == 1 || m == 1) ? addzero(n) : n);
-      if (ent == 0) {
+      var ndis = ((enterType_ == 1 || targetSelector_ == 1) ? addzero(n) : n);
+      if (enterType_ == 0) {
         ndis = ((l == 1 || l == 4) ? addzero(n) : n);
       }
       display(document.NONO.funct, ndis);
@@ -211,15 +266,17 @@ function push(button) {         //ボタン処理//
 }
 //
 function period() {         //時刻合わせ及び小数点処理//
-  if (fn == 1 && step == 0) {
-    ent = step = 1;
-    et = 1;
+  if (fn == 1 && enterSteps_ == 0) {
+    enterType_ = enterSteps_ = 1;
+    et_ = 1;
     n = "";
-    p = fn = m = 0;
+    p = 0;
+    fn = 0;
+    targetSelector_ = 0;
     display(document.NONO.funct, clock(0));
     fu(15);
   } else {
-    if (p == 0 && (m == 2 || m == 5)) {
+    if (p == 0 && (targetSelector_ == 2 || targetSelector_ == 5)) {
       p = 1;
       n = n  + ".";
     }
@@ -230,93 +287,93 @@ function enter() {         //エンター処理//
   if (n != "") {
     n = eval(n);
   }
-  if (ent == 1) {         //時刻あわせ
+  if (enterType_ == 1) {         //時刻あわせ
     if (n != "" && (n%100) < 60 && (n % 10000) < 6000 && n < 120000) {
       clearTimeout(s10);
       s10 = setTimeout("chousei()", 7000);
-      tim = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100 - 1;
+      time_ = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100 - 1;
       clearTimeout(s);
       second();
     }
-    ent = step = l = m = 0;
+    enterType_ = enterSteps_ = l = targetSelector_ = 0;
   }
-  if (ent == 0 && f2 == 0) {         //通常の入力
+  if (enterType_ == 0 && f2Mode_ == 0) {         //通常の入力
     hanni();
   }
-  if (ent == 2) {         //オド
-    step--;
+  if (enterType_ == 2) {         //オド
+    enterSteps_--;
     if (n == "") {     ///////
     } else {
-      if (step == 3 && n != 0) {
-        i = strp[0] / n;
+      if (enterSteps_ == 3 && n != 0) {
+        var i = strp_[0] / n;
         if (i >= .3 || i <= 3) {
-          k = i;
+          kHosei_ = i;
         }
       } else {
         hanni();
       }
     }
-    m = 4 - step + (step == 1 ? 2 : 0);
-    power = 3;   //オド処理までkを入力できない様にする
-    if (step == 0) {
-      ent = l = m = 0;
-      d = clock(tim);
+    targetSelector_ = 4 - enterSteps_ + (enterSteps_ == 1 ? 2 : 0);
+    power_ = 3;   //オド処理までkを入力できない様にする
+    if (enterSteps_ == 0) {
+      enterType_ = l = targetSelector_ = 0;
+      d = clock(time_);
     }
-    fu(31 - step);
+    fu(31 - enterSteps_);
   }
-  if (ent == 3) {         //チェック////////
-    step--;
-    et = (step == 2 ? 1 : 0);
-    m = 4 - step;
-    if (step <= 0) {
-      ent = l = m = 0;
-      d = clock(tim);
+  if (enterType_ == 3) {         //チェック////////
+    enterSteps_--;
+    et_ = (enterSteps_ == 2 ? 1 : 0);
+    targetSelector_ = 4 - enterSteps_;
+    if (enterSteps_ <= 0) {
+      enterType_ = l = targetSelector_ = 0;
+      d = clock(time_);
     }
-    fu(25 - step);
+    fu(25 - enterSteps_);
   }
-  if (ent == 4) {         //パスコン////
-    step--;
-    if (step == 1) {
-      m = 2;
+  if (enterType_ == 4) {         //パスコン////
+    enterSteps_--;
+    if (enterSteps_ == 1) {
+      targetSelector_ = 2;
       hanni();
-      et = 0
-      m = 3;
+      et_ = 0
+      targetSelector_ = 3;
     }
-    if (step == 0) {
-      ent = l = m = 0;
-      d = clock(tim);
+    if (enterSteps_ == 0) {
+      enterType_ = l = targetSelector_ = 0;
+      d = clock(time_);
     }
-    fu(27 - step);
+    fu(27 - enterSteps_);
   }
   cl();   //共通掃除
 }
 //
 function hanni() {         //入力値が正しいか？
-  chr = "a" + n;
+  var chr = "a" + n;
   if (chr.length > 1) {  //forNN3
-    if (m == 1) {         //ＳＴ入力
+    if (targetSelector_ == 1) {         //ＳＴ入力
       if ((n % 100) < 60 && (n % 10000) < 6000 && n < 120000) {
-        st = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100;
-        d = clock(st);
+        startTime_ = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100;
+        d = clock(startTime_);
       }
     }
-    if (m == 2) {         //アベ入力
+    if (targetSelector_ == 2) {         //アベ入力
       if (n >= 5 && n < 200) {
-        ave = d = n;
+        ave_ = d = n;
       }
     }
-    if (m == 4) {         //レスコン入力
+    if (targetSelector_ == 4) {         //レスコン入力
       if ((n % 100) < 60 && (n % 10000) < 6000 && n < 20000) {      
-        rc = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100;
-        d = clock(rc);
+        resCon_ = ((n % 100) * 40 + (n % 10000) * 24 + n * 36) / 100;
+        d = clock(resCon_);
       }
     }
-    if (m == 5) {         //Ｋ入力
+    if (targetSelector_ == 5) {         //Ｋ入力
       if (n > .3 && n < 3) {
-        k = d = n;
+        kHosei_ = d = n;
       }
     }
-    if (m == 6) {        //補正距離入力
+    if (targetSelector_ == 6) {        //補正距離入力
       if (n >= 0) {
         tp = d = n;
       }
@@ -325,36 +382,37 @@ function hanni() {         //入力値が正しいか？
 }
 //
 function chira(kazu) {         //表示切替//
-  num = (l == 0 ? tim : 0)
-    + (l == 1 ? st : 0)
-    + (l == 2 ? ave : 0)
-    + (l == 3 ? trp : 0)
-    + (l == 4 ? rc : 0)
-    + (l == 5 ? k : 0)
+  var num = (l == 0 ? time_ : 0)
+    + (l == 1 ? startTime_ : 0)
+    + (l == 2 ? ave_ : 0)
+    + (l == 3 ? trp_ : 0)
+    + (l == 4 ? resCon_ : 0)
+    + (l == 5 ? kHosei_ : 0)
     + (l == 6 ? tp : 0)
     + (l == 7 ? smp : 0);
   num = (l < 2 || l == 4) ? clock(num) : num;
   
-  if (m != l && step == 0 && f2 == 0) {         //インジゲーター切り替え
+  if (targetSelector_ != l && enterSteps_ == 0 && f2Mode_ == 0) {         //インジゲーター切り替え
     d = num;
     display(document.NONO.funct, d)
-    m = l;
-    document.NONO.lt[m].checked = true;
+    targetSelector_ = l;
+    document.NONO.lt[targetSelector_].checked = true;
     n = "";
-    p = fn = 0;
-    fu(m + 4);
+    p = 0;
+    fn = 0;
+    fu(targetSelector_ + 4);
   } else {
-    if (f2 == 0) {
-      document.NONO.lt[m].checked = true;
+    if (f2Mode_ == 0) {
+      document.NONO.lt[targetSelector_].checked = true;
     }
   }
 }
 //
 function checkpoint() {         //チェック処理
-  ent = step = 3;
-  st = tim;
-  m = 1;
-  et = 0;
+  enterType_ = enterSteps_ = 3;
+  startTime_ = time_;
+  targetSelector_ = 1;
+  et_ = 0;
   trc = 0;
   trpcl();
   cl();
@@ -362,54 +420,56 @@ function checkpoint() {         //チェック処理
 //
 function susumu(keta) {         //マップ増加処理
   // 20050210 : for IE6
-  map +=  dir * keta;
-  trp +=  dir * keta;
+  map_ +=  tripSensorDir_ * keta;
+  trp_ +=  tripSensorDir_ * keta;
 }
 //
 function pathcon() {         //パスコン
-  st += kirisute(3.6 * trp / ave, 0);
-  st = st % 43200;
-  ent = 4;
-  step = 2;
-  et = 1;
-  m = 2;
+  startTime_ += kirisute(3.6 * trp_ / ave_, 0);
+  startTime_ = startTime_ % 43200;
+  enterType_ = 4;
+  enterSteps_ = 2;
+  et_ = 1;
+  targetSelector_ = 2;
   trpcl();
   cl();
 }
 //
 function trpcl() {         //トリップクリア
-  strp[2] = strp[1];
-  strp[1] = strp[0];
-  strp[0] = trp;
-  trp = 0;
-  fn = f2 = 0;
+  strp_[2] = strp_[1];
+  strp_[1] = strp_[0];
+  strp_[0] = trp_;
+  trp_ = 0;
+  fn = 0;
+  f2Mode_ = 0;
 }
 //
 function cl() {         //入力値クリア//
   n = "";
-  p = fn = 0;
-  if (ent == 0) {
+  p = 0;
+  fn = 0;
+  if (enterType_ == 0) {
     display(document.NONO.funct, d);
     clearTimeout(ss);
-    document.NONO.lt[m].checked = true;
+    document.NONO.lt[targetSelector_].checked = true;
   }
-  if (ent == 1) {
+  if (enterType_ == 1) {
     display(document.NONO.funct, "000000");
     fu(13);
   }
-  if (ent >= 2) {
-    d = (m == 1 ? clock(st) : "")
-    + (m == 2 ? ave : "")
-    + (m == 3 ? strp[0] : "")
-    + (m == 5 ? k : "")
-    + (m == 6 ? strp[0] : "");
+  if (enterType_ >= 2) {
+    d = (targetSelector_ == 1 ? clock(startTime_) : "")
+    + (targetSelector_ == 2 ? ave_ : "")
+    + (targetSelector_ == 3 ? strp_[0] : "")
+    + (targetSelector_ == 5 ? kHosei_ : "")
+    + (targetSelector_ == 6 ? strp_[0] : "");
     display(document.NONO.funct, d);
   }
 }
 //
 function kirisute (atai, keta) {         //小数の桁揃え/////
-  u = atai;
-  for (i = 0; i < keta; i++) {
+  var u = atai;
+  for (var i = 0; i < keta; i++) {
   u = 10 * u;
   }
   u = Math.floor(u);   //-1.1が2になる？
@@ -425,7 +485,7 @@ function about() {
 ////////////////////////////////////////
 //		使い方表示
 ////////////////////////////////////////
-ji = new Array(33);
+var ji = new Array(33);
 ji[0] = "FINALを忘れずに！メモし、STとチェックカードの値を照合し（間違っていれば修正し）、「ENT」を押す。";
 ji[1] = "ＰＣ処理。新しいアベを入れ、「ENT」を押す。";
 ji[2] = "MAPscの値をリセットしました。次のコマ図を読み上げ、余裕ができればSMPに入った値をメモする。";
@@ -461,7 +521,7 @@ ji[31] = "オド処理終了！";
 ji[32] = "";
 
 function fu(bnum) {
-  i = bnum;
+  //i = bnum;
   //window.status = ji[i];
   // window.defaultstatus = ji[i];
   document.getElementById("status-box").textContent = ji[bnum];
